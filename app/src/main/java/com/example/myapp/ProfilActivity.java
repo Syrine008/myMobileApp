cbf0f902dd17250b4f93cdfde5f2a1bcde1c016d
Syrine008 <sirine.ghorbel29@gmail.com>
2024-09-07 19:13:18 +0100

    done : navigationDrawer for ProfilActivity

--- app/src/main/java/com/example/myapp/ProfilActivity.java
+++ app/src/main/java/com/example/myapp/ProfilActivity.java
@@ -75,7 +75,6 @@ public class ProfilActivity extends AppCompatActivity implements NavigationView.
                 startActivity(new Intent(ProfilActivity.this, HomeActivity.class));
             }
             return true;
-
         });
 
         databaseReference.addValueEventListener(new ValueEventListener() {
